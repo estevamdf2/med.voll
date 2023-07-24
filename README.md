be6adc084943757a4bbc2fe62b3f457ec4f1118e
Marcos Sousa <>
2023-07-24 07:33:28 -0300

    Correção de erro ao chamar endpoints da aplicação, inclusão de dependências para usar banco de dados para persistência

---
+++ README.md
@@ -0,0 +1,15 @@
+#MED.VOL
+
+Aplicação de estudo do Spring boot 3. Curso da Alura
+
+Banco de dados utilizados MySql.
+
+## Docker
+
+Use o docker para startar o banco de dados da aplicação. Comando para executar o database
+
+`
+docker run --name mysql-alura -e MYSQL_ROOT_PASSWORD=root -d mysql:8.0.33
+`
+
+Nome do database: *medvoll_api*
--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -2,8 +2,10 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "med.voll")
 public class ApiApplication {
 
 	public static void main(String[] args) {
