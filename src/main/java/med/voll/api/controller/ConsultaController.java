297dcae3953bdfa2adefc65739cee7c0d0c68889
Marcos Sousa <>
2023-09-01 14:35:33 -0300

    Aula 03 documentação com SpringDoc.

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,7 +1,12 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import jdk.jfr.Description;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
@@ -19,6 +25,9 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Realiza a marcação de consultas médicas", tags = {"consulta"})
+    @ApiResponse(responseCode = "400", description = "Id do paciente informado não existe!")
+    @ApiResponse(responseCode = "400", description = "Id do médico informado não existe!")
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
         agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
@@ -26,6 +35,7 @@ public class ConsultaController {
 
     @DeleteMapping
     @Transactional
+    @Operation(summary = "Realiza o cancelamento de consultas médicas")
     public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados){
         agenda.cancelar(dados);
         return ResponseEntity.ok(new DadosCancelamentoConsulta(null, null, null));
